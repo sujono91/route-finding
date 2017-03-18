0318f7f88982eaad42cdcb927a604fef3de03013
sujono <sujono@gdplabs.id>
2017-03-18 18:04:11 +0700

    Change directory gzip

--- web.js
+++ web.js
@@ -3,5 +3,5 @@ var express = require('express');
 var app = express();
 
 app.use(express.logger('dev'));
-app.use(gzippo.staticGzip('' + __dirname + '/dist'));
-app.listen(process.env.PORT || 5000);
+app.use(gzippo.staticGzip('' + __dirname + '/build'));
+app.listen(process.env.PORT || 3000);
